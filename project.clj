4ea4c86a9ebedf6ddffdc0e255ac06039f5310d6
Erick Brower <erick@berecruited.com>
2014-02-22 21:40:42 -0800

    Added ragtime and configured it.

--- project.clj
+++ project.clj
@@ -7,21 +7,38 @@
                  [io.pedestal/pedestal.service "0.2.2"]
                  [io.pedestal/pedestal.service-tools "0.2.2"]
                  [environ "0.4.0"]
+                 [ragtime/ragtime.sql.files "0.3.4"]
                  [postgresql/postgresql "9.1-901.jdbc4"]
                  ;; Remove this line and uncomment the next line to
                  ;; use Tomcat instead of Jetty:
                  [io.pedestal/pedestal.jetty "0.2.2"]
                  ;; [io.pedestal/pedestal.tomcat "0.2.2"]
                  ]
+  :ragtime {:migrations ragtime.sql.files/migrations
+            :database ((keyword (or (System/getenv "LEIN_ENV") "dev"))
+                       {:dev "jdbc:postgresql://localhost/authority_dev?user=postgres"
+                        :test "jdbc:postgresql://localhost/authority_test?user=postgres"})}
   :profiles
   {:dev 
-   {:env {:dev true}}
+   {:env {:dev true
+          :db-host "localhost"
+          :db-subprotocol "postgresql"
+          :db-port "5432"
+          :db-name "authority_dev"
+          :db-user "postgres"
+          :db-pass ""}}
    :test 
-   {:env {:test true}}
+   {:env {:test true
+          :db-host "localhost"
+          :db-subprotocol "postgresql"
+          :db-port "5432"
+          :db-name "authority_test"
+          :db-user "postgres"
+          :db-pass ""}}
    :production 
    {:env {:production true}}}
   :plugins 
-  [[lein-environ "0.4.0"]]
+  [[lein-environ "0.4.0"][ragtime/ragtime.lein "0.3.4"]]
   :min-lein-version "2.0.0"
   :resource-paths ["config", "resources"]
   :aliases {"run-dev" ["trampoline" "run" "-m" "authority.server/run-dev"]}
